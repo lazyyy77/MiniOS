6a04bbf46133bda9875d7636b93d9bad3d32f601
lazyyy <1669335639@qq.com>
2024-11-30 19:45:25 +0800

    [INIT][lab4]

---
+++ src/lab4/arch/riscv/kernel/trap.c
@@ -0,0 +1,44 @@
+#include "stdint.h"
+#include "printk.h"
+#include "clock.h"
+#include "defs.h"
+#include "proc.h"
+#include <stdint.h>
+
+void trap_handler(uint64_t scause, uint64_t sepc) {
+
+    // char high = (scause >> 60) & 0xf;
+    // char low = scause & 0xf;
+    // const char* ch = high;
+    // const char* cl = low;
+    // printk(ch);
+    // printk(cl);
+    uint64_t myscause;
+    
+    // asm volatile("csrr %0, scause" : "=r"(myscause)); 
+    // myscause = csr_read(scause);
+    myscause = scause;
+    if (myscause & (1ull << 63)) {
+        if (myscause & 0x5) {
+            // printk("[S] is time interrupt\n");
+            clock_set_next_event();
+            do_timer();
+            // uint64_t ss = csr_read(sstatus);
+            // printk("sstatus is: %ld", ss);
+            // csr_write(sscratch, 0x0001001001001000);
+            return;
+        } else {
+            printk("not time interrupt\n");
+            return;
+        }
+    } else {
+        printk("not interrupt\n");
+        return;
+    }
+    // 通过 `scause` 判断 trap 类型
+    // 如果是 interrupt 判断是否是 timer interrupt
+    // 如果是 timer interrupt 则打印输出相关信息，并通过 `clock_set_next_event()` 设置下一次时钟中断
+    // `clock_set_next_event()` 见 4.3.4 节
+    // 其他 interrupt / exception 可以直接忽略，推荐打印出来供以后调试
+    return;
+}
